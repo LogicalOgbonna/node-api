b92e2277aa088d34ae1a2d266bcfef48f9dea71f
LogicalOgbonna <arinzeogbo@gmail.com>
2018-11-21 15:45:39 +0100

    initial commit, created a server and a get request

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (res, req) => {
+    req.status(200).send("Hello world!");
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Open at localhost:3000");
+});
